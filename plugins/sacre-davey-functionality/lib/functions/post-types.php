0936e60a8f1c537d01daa6cad3e95baf80e3c573
fergmac <f.macconnell@gmail.com>
2016-09-06 15:58:58 -0700

    create Sectors/Projects custom post types

--- plugins/sacre-davey-functionality/lib/functions/post-types.php
+++ plugins/sacre-davey-functionality/lib/functions/post-types.php
@@ -6,3 +6,114 @@
  */
 
 // Add your custom post types here...
+
+// Register Custom Post Type
+function sectors() {
+
+	$labels = array(
+		'name'                  => 'Sectors',
+		'singular_name'         => 'Sector',
+		'menu_name'             => 'Sectors',
+		'name_admin_bar'        => 'Sector',
+		'archives'              => 'Sector Archives',
+		'parent_item_colon'     => 'Parent Sector:',
+		'all_items'             => 'All Sectors',
+		'add_new_item'          => 'Add New Sector',
+		'add_new'               => 'Add New',
+		'new_item'              => 'New Sector',
+		'edit_item'             => 'Edit Sector',
+		'update_item'           => 'Update Sector',
+		'view_item'             => 'View Sector',
+		'search_items'          => 'Search Sector',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into sector',
+		'uploaded_to_this_item' => 'Uploaded to this sector',
+		'items_list'            => 'Sectors list',
+		'items_list_navigation' => 'Sectors list navigation',
+		'filter_items_list'     => 'Filter sectors list',
+	);
+	$args = array(
+		'label'                 => 'Sector',
+		'description'           => 'Post Type Description',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-hammer',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => 'sectors',		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'sector', $args );
+
+}
+add_action( 'init', 'sectors', 0 );
+
+
+// Register Custom Post Type
+function Projects() {
+
+	$labels = array(
+		'name'                  => 'Projects',
+		'singular_name'         => 'Project',
+		'menu_name'             => 'Projects',
+		'name_admin_bar'        => 'Project',
+		'archives'              => 'Project Archives',
+		'parent_item_colon'     => 'Parent Project:',
+		'all_items'             => 'All Projects',
+		'add_new_item'          => 'Add New Project',
+		'add_new'               => 'Add New',
+		'new_item'              => 'New Project',
+		'edit_item'             => 'Edit Project',
+		'update_item'           => 'Update Project',
+		'view_item'             => 'View Project',
+		'search_items'          => 'Search Project',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into project',
+		'uploaded_to_this_item' => 'Uploaded to this project',
+		'items_list'            => 'projects list',
+		'items_list_navigation' => 'Projects list navigation',
+		'filter_items_list'     => 'Filter projects list',
+	);
+	$args = array(
+		'label'                 => 'Project',
+		'description'           => 'Post Type Description',
+		'labels'                => $labels,
+		'supports'              => array( ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-clipboard',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'projects', $args );
+
+}
+add_action( 'init', 'Projects', 0 );
