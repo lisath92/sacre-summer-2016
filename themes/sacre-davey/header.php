39e8e0e77fdb156e460fcf2ce373aa9e897d6f69
lisath92 <lisa.hsu.419@gmail.com>
2016-09-07 00:06:18 -0700

    add logo and hamburger menu

--- themes/sacre-davey/header.php
+++ themes/sacre-davey/header.php
@@ -27,8 +27,12 @@
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
+					<div class="logo">
+					<a href="<?php echo esc_url(home_url('/')); ?>" />
+					<img src="<?php echo get_template_directory_uri().'/assets/images/mobile-logo.png'?>" >
+					</div>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					<i class="fa fa-bars" aria-hidden="true"></i>
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
 
