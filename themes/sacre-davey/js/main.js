8a225954ee94fa4353eaa6fa85e1c98f0b3f8ef5
lisath92 <lisa.hsu.419@gmail.com>
2016-09-07 14:09:24 -0700

    add menu slider

--- themes/sacre-davey/js/main.js
+++ themes/sacre-davey/js/main.js
@@ -1,3 +1,8 @@
 (function($) {
-   
+
+  var mainMenu = $('.menu-main-navigation-container');
+   $('.fa-bars').on('click', function(){
+    event.preventDefault();
+    mainMenu.slideToggle('slow');
+   })
 })(jQuery);
