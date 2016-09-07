0b84019207f9b0edc11d8a13da79f93c23cb348d
lisath92 <lisa.hsu.419@gmail.com>
2016-09-06 22:59:31 -0700

    add jquery

--- themes/sacre-davey/functions.php
+++ themes/sacre-davey/functions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * RED Starter Theme functions and definitions.
+ * Sacre Davey Theme functions and definitions.
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
@@ -82,16 +82,20 @@ add_filter( 'stylesheet_uri', 'red_starter_minified_css', 10, 2 );
 /**
  * Enqueue scripts and styles.
  */
-function red_starter_scripts() {
+function sacre_davey_scripts() {
 	wp_enqueue_style( 'red-starter-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
+
+	wp_enqueue_script( 'jquery');
+	
+	wp_enqueue_script( 'sacre-davey-font-awesome', 'https://use.fontawesome.com/2c227b0ccc.js', array(), '4.6.3', false);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-add_action( 'wp_enqueue_scripts', 'red_starter_scripts' );
+add_action( 'wp_enqueue_scripts', 'sacre_davey_scripts' );
 
 /**
  * Custom template tags for this theme.
