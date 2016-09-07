67967c29fc2b1130d08bd654fb73c1e0ccd18c05
lisath92 <lisa.hsu.419@gmail.com>
2016-09-07 11:11:43 -0700

    add logged in nav bar

--- themes/sacre-davey/header.php
+++ themes/sacre-davey/header.php
@@ -22,11 +22,12 @@
 
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<div class="logo">
 					<a href="<?php echo esc_url(home_url('/')); ?>" />
 					<img src="<?php echo get_template_directory_uri().'/assets/images/mobile-logo.png'?>" >
