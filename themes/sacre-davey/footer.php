2956eb1a1251dedcbd468a861a973bb71060ea62
lisath92 <lisa.hsu.419@gmail.com>
2016-09-08 09:31:57 -0700

    add contact us button and update link and link hover colors

--- themes/sacre-davey/footer.php
+++ themes/sacre-davey/footer.php
@@ -7,26 +7,32 @@
 
 ?>
 
-			</div><!-- #content -->
+</div><!-- #content -->
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<section class="site-info container">
-					<div class="footer-logo">
-            <img src="<?php echo get_template_directory_uri() .'/assets/images/footer-logo.png'?>" alt="Sacre Davey footer logo"/>    
-          </div>
-          <div class="social-media">
-          <h1>Follow Us</h1>
-            <i class="fa fa-linkedin" aria-hidden="true"></i>
-            <i class="fa fa-facebook" aria-hidden="true"></i>
-            <i class="fa fa-youtube" aria-hidden="true"></i>
-           <!--  <img src="<?php //echo get_template_directory_uri() .'/assets/images/footer-youtube.png'?>" alt="YouTube icon"> -->
-          </div>
+<footer id="colophon" class="site-footer" role="contentinfo">
+  <section class="site-info container">
+   <div class="footer-logo">
+    <img src="<?php echo get_template_directory_uri() .'/assets/images/footer-logo.png'?>" alt="Sacre Davey footer logo"/>    
+  </div>
+  <div class="more-info">
+    <h1>Need more info?</h1>
+    <a href="/sacre-davey/contact">
+    <button type="submit">Contact Us</button>
+    </a>
+  </div>
+  <div class="social-media">
+    <h1>Follow Us</h1>
+    <i class="fa fa-linkedin" aria-hidden="true"></i>
+    <i class="fa fa-facebook" aria-hidden="true"></i>
+    <i class="fa fa-youtube" aria-hidden="true"></i>
+  </div>
 
-				</section><!-- .site-info -->
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
 
-		<?php wp_footer(); ?>
+</section><!-- .site-info -->
+</footer><!-- #colophon -->
+</div><!-- #page -->
 
-	</body>
+<?php wp_footer(); ?>
+
+</body>
 </html>
