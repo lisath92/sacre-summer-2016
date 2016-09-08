031657e61f38d2d806cb9a68279bd7759deece9a
lisath92 <lisa.hsu.419@gmail.com>
2016-09-08 00:19:43 -0700

    add and style footer, mobile-first

--- themes/sacre-davey/footer.php
+++ themes/sacre-davey/footer.php
@@ -10,9 +10,19 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
+				<section class="site-info container">
+					<div class="footer-logo">
+            <img src="<?php echo get_template_directory_uri() .'/assets/images/footer-logo.png'?>" alt="Sacre Davey footer logo"/>    
+          </div>
+          <div class="social-media">
+          <h1>Follow Us</h1>
+            <i class="fa fa-linkedin" aria-hidden="true"></i>
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+            <i class="fa fa-youtube" aria-hidden="true"></i>
+           <!--  <img src="<?php //echo get_template_directory_uri() .'/assets/images/footer-youtube.png'?>" alt="YouTube icon"> -->
+          </div>
+
+				</section><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
