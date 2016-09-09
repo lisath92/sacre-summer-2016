b5f0fa3abe6979febd501194cf781fa2bdb6de03
fergmac <f.macconnell@gmail.com>
2016-09-09 11:36:34 -0700

    input and style opportunities list on page-careers

--- themes/sacre-davey/page-careers.php
+++ themes/sacre-davey/page-careers.php
@@ -22,10 +22,28 @@ get_header(); ?>
 						<p>Our established engineering firm is GROWING! With our home office in North Vancouver and regional offices across Canada and the Western United States, we are looking for dynamic, well rounded individuals to join our team. These positions require an individual who is highly motivated, self-directed and has amazing attention to detail. We’re always looking for talented individuals to join our team in making a difference.</p>
 					</div>
 					<div class="life-work-banner">
-					<<h2>Join us and put a little more life in your life/work</h2>
-					<<p>Sacré Davey is our home not just where we work, we are a family and we want you to be apart of it. Come join us for a challenging yet rewarding work experience where you'll be able to learn and move your career forward.</p>
+					<h2>Join us and put a little more life in your life/work</h2>
+					<p>Sacré Davey is our home not just where we work, we are a family and we want you to be apart of it. Come join us for a challenging yet rewarding work experience where you'll be able to learn and move your career forward.</p>
 					</div>
 					<div class="opportunities-wrapper">
+					<h1 class="opportunities-banner">Opportunities</h1>
+					<ul class="canadian-opportunities">
+						<li class="opportunities-title">Calgary, Canada
+							<ul>
+								<li>Intermediate Process Engineer</li>
+							</ul>
+						</li>
+					</ul>
+					
+					<ul class="american-opportunities">
+						<li class="opportunities-title">Seattle, USA
+							<ul>
+								<li>Office &amp; Finance Manager</li>
+								<li>Senior Process Engineer, SDE3</li>
+							</ul>
+						</li>
+					</ul>
+
 						
 					</div>
 					<?php the_content(); ?>
