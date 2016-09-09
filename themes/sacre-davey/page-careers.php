b537dfb89cb56c5de091ee99e17995b7cf41955a
fergmac <f.macconnell@gmail.com>
2016-09-08 22:07:31 -0700

    add careers background image to header

--- themes/sacre-davey/page-careers.php
+++ themes/sacre-davey/page-careers.php
@@ -5,34 +5,29 @@
  * @package Sacre Davey Theme
  */
 
-?>
-<header class="entry-header">
-	<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-</header><!-- .entry-header -->
+get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 		
-		<?php while ( have_posts() ) : the_post(); ?>
+		
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<header class="entry-header">
-					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+					
 				</header><!-- .entry-header -->
 
 				<div class="entry-content">
+					<div class="growing-text">
+						<p>Our established engineering firm is GROWING! With our home office in North Vancouver and regional offices across Canada and the Western United States, we are looking for dynamic, well rounded individuals to join our team. These positions require an individual who is highly motivated, self-directed and has amazing attention to detail. We’re always looking for talented individuals to join our team in making a difference.</p>
+					</div>
 					<?php the_content(); ?>
-					<?php
-					wp_link_pages( array(
-						'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-						'after'  => '</div>',
-						) );
-						?>
+
 					</div><!-- .entry-content -->
 				</article><!-- #post-## -->
-			<?php endwhile; // End of the loop. ?>
+		
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 	
-	<?php get_footer(); ?>
+
