fc4b45312d151a5da4d0425696e1253d06bd01e2
fergmac <f.macconnell@gmail.com>
2016-09-09 11:24:01 -0700

    add life-work-banner to page-careers

--- themes/sacre-davey/page-careers.php
+++ themes/sacre-davey/page-careers.php
@@ -21,6 +21,13 @@ get_header(); ?>
 					<div class="growing-text">
 						<p>Our established engineering firm is GROWING! With our home office in North Vancouver and regional offices across Canada and the Western United States, we are looking for dynamic, well rounded individuals to join our team. These positions require an individual who is highly motivated, self-directed and has amazing attention to detail. We’re always looking for talented individuals to join our team in making a difference.</p>
 					</div>
+					<div class="life-work-banner">
+					<<h2>Join us and put a little more life in your life/work</h2>
+					<<p>Sacré Davey is our home not just where we work, we are a family and we want you to be apart of it. Come join us for a challenging yet rewarding work experience where you'll be able to learn and move your career forward.</p>
+					</div>
+					<div class="opportunities-wrapper">
+						
+					</div>
 					<?php the_content(); ?>
 
 					</div><!-- .entry-content -->
