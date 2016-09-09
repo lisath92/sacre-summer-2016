37cbf25011921a2d3b9eb0f5eafe40df9bdd8cf5
fergmac <f.macconnell@gmail.com>
2016-09-09 16:06:15 -0700

    hardcode oil-gas lists to be flexed

--- themes/sacre-davey/page-oil-gas.php
+++ themes/sacre-davey/page-oil-gas.php
@@ -23,18 +23,84 @@ get_header(); ?>
 					<p>The oil &amp; gas industry brings various challenges. Reserves can diminish and product demand fluctuates - reliability has become more critical than ever. And you'll find our people working on the full spectrum of oil &amp; gas production and support services all over the globe.</p>
 				</header><!-- .entry-header -->
 				<div class="entry-content">
-				<div class="what-we-offer-wrapper">
-					<h1>What We Offer</h1>
-					<ul class="what-we-offer-list">
-					
-					<li>Our oil and gas section focuses on your company and your needs - from front end consulting work to commissioning.We deliver projects on time, safely and in the most cost efficient manner possible.</li>
+					<div class="what-we-offer-wrapper">
+						<h1>What We Offer</h1>
+						<ul class="what-we-offer-list">
+							
+							<li>Our oil and gas section focuses on your company and your needs - from front end consulting work to commissioning.We deliver projects on time, safely and in the most cost efficient manner possible.</li>
 
-					<li>With multiple offices based around industry Centres of Excellence, we create an appropriately resourced and right-sized team for each project.</li>
-				
-					<li>From tie-ins to processing and beyond, we have a strong track record of meeting our clients’ needs to ensure quality, reduce cost, improve schedule, and maximize safety, compliance and performance goals.</li>
-					</ul>
-				</div>
+							<li>With multiple offices based around industry Centres of Excellence, we create an appropriately resourced and right-sized team for each project.</li>
+							
+							<li>From tie-ins to processing and beyond, we have a strong track record of meeting our clients’ needs to ensure quality, reduce cost, improve schedule, and maximize safety, compliance and performance goals.</li>
+						</ul>
+					</div>
+					<div class="oil-gas-dropdown-wrapper">
+						<div class="conventional-oil-wrapper">
+							<h1>Conventional Oil</h1>
+							<ul>
+								<li>Distillation</li>
+								<li>FGD reduction units</li>
+								<li>SCR reduction units</li>
+								<li>NOx reduction units</li>
+								<li>CO2 reduction units</li>
+								<li>Over pressure protection &amp; flare systems </li>
+								<li>Recovery</li>
+								<li>Blending, terminalling &amp; transfer reforming</li>
+								<li>Hydrogen plants</li>
+								<li>Hydrocracking</li>
+								<li>Hydrotreating </li>
+								<li>Isomerization units</li>
+								<li>Gathering systems &amp; conditioning</li>
+							</ul>
+							<ul>
+								<li>Lubricants refining</li>
+								<li>Oily water treatment</li>
+								<li>Sulphur recovery</li>
+								<li>Solvent de-asphalting &amp; visbreaking</li>
+								<li>Cooling tower / cooling water systems</li>
+								<li>Steam generation</li>
+								<li>Cogeneration</li>
+								<li>Renewable fuels blending</li>
+								<li>Ethanol</li>
+								<li>Biodiesel</li>
+								<li>Sour water stripping</li>
+								<li>Heat integration</li>
+								<li>Pressure regulation stations</li>
+							</ul>
+						</div>
+						<div class="natural-gas-wrapper">
+							<h1>Natural Gas</h1>
+							<ul>
+								<li>LNGs, NGLs, ethane, propane, butane, condensates, crude oils</li>
+								<li>Vaporization</li>
+								<li>Liquefaction</li>
+								<li>Dehydration &amp; sweetening</li>
+								<li>Liquid extraction &amp; fractionation</li>
+								<li>Sulphur recovery</li>
+							</ul>
+							<ul>
+								<li>Pipelines</li>
+								<li>Pipeline integrity &amp; maintenance</li>
+								<li>Pumping and compressor stations</li>
+								<li>Custody transfer systems</li>
+								<li>Terminalling, Storage &amp; Blending</li>
+							</ul>
+						</div>
+						<div class="oil-sands-wrapper">
+							<h1>Oil Sands Recovery</h1>
+							<ul>
+								<li>Conventional oil production / processing systems</li>
+								<li>Primary heavy oil production</li>
+								<li>Steam assisted gravity drainage (SAGD) systems</li>
+							</ul>
+							<ul>
+								<li>Steam generation and piping systems</li>
+								<li>Material conveying and equipment design</li>
+								<li>Tankage and transfer</li>
+							</ul>
+						</div>
 
+					</div>
 					<?php the_content(); ?>
 
 					
