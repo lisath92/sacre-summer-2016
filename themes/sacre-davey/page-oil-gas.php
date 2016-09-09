5fc59c5760699b239ccc260b7bffa9955c4738be
fergmac <f.macconnell@gmail.com>
2016-09-09 15:39:36 -0700

    create and style what-we-off-list on oil&gas page

--- themes/sacre-davey/page-oil-gas.php
+++ themes/sacre-davey/page-oil-gas.php
@@ -5,21 +5,45 @@
  * @package Sacre Davey Theme
  */
 
- ?>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 		
-			<?php while ( have_posts() ) : the_post(); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				</header><!-- .entry-header -->
+				<div class="entry-content">
+					<p>Egg cartons and ice cube trays, umbrellas and polar fleece, toothbrushes and eye glasses, heart valves, syringes and oxygen masks – what do all these different products have in common? They are all petroleum-based.</p>
 
-			<?php endwhile; // End of the loop. ?>
+					<p>For 30 years, Sacré-Davey Engineering has been providing integrated solutions to the oil &amp; gas industry, allowing these companies to thrive and continue enriching our lives.</p> 
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-	
+					<p>The oil &amp; gas industry brings various challenges. Reserves can diminish and product demand fluctuates - reliability has become more critical than ever. And you'll find our people working on the full spectrum of oil &amp; gas production and support services all over the globe.</p>
+				<div class="what-we-offer-wrapper">
+					<ul class="what-we-offer-list">
+					
+					<li>Our oil and gas section focuses on your company and your needs - from front end consulting work to commissioning.We deliver projects on time, safely and in the most cost efficient manner possible.</li>
+
+					<li>With multiple offices based around industry Centres of Excellence, we create an appropriately resourced and right-sized team for each project.</li>
+				
+					<li>From tie-ins to processing and beyond, we have a strong track record of meeting our clients’ needs to ensure quality, reduce cost, improve schedule, and maximize safety, compliance and performance goals.</li>
+					</ul>
+				</div>
+
+					<?php the_content(); ?>
+
+					
+				</div><!-- .entry-content -->
+			</article><!-- #post-## -->
+
+
+		<?php endwhile; // End of the loop. ?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
 <?php get_footer(); ?>
