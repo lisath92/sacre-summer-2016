3309de9a5e148fe5d8734c010d3de18e7541de13
fergmac <f.macconnell@gmail.com>
2016-09-09 15:50:22 -0700

    restyle oil-gas header image

--- themes/sacre-davey/page-oil-gas.php
+++ themes/sacre-davey/page-oil-gas.php
@@ -16,14 +16,15 @@ get_header(); ?>
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<header class="entry-header">
 					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-				</header><!-- .entry-header -->
-				<div class="entry-content">
 					<p>Egg cartons and ice cube trays, umbrellas and polar fleece, toothbrushes and eye glasses, heart valves, syringes and oxygen masks – what do all these different products have in common? They are all petroleum-based.</p>
 
 					<p>For 30 years, Sacré-Davey Engineering has been providing integrated solutions to the oil &amp; gas industry, allowing these companies to thrive and continue enriching our lives.</p> 
 
 					<p>The oil &amp; gas industry brings various challenges. Reserves can diminish and product demand fluctuates - reliability has become more critical than ever. And you'll find our people working on the full spectrum of oil &amp; gas production and support services all over the globe.</p>
+				</header><!-- .entry-header -->
+				<div class="entry-content">
 				<div class="what-we-offer-wrapper">
+					<h1>What We Offer</h1>
 					<ul class="what-we-offer-list">
 					
 					<li>Our oil and gas section focuses on your company and your needs - from front end consulting work to commissioning.We deliver projects on time, safely and in the most cost efficient manner possible.</li>
