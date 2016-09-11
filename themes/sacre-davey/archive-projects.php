97c69f177d369fddb6d654277dd199dab580fb21
fergmac <f.macconnell@gmail.com>
2016-09-11 15:16:46 -0700

    flex archive projects page 5 projects to a row

--- themes/sacre-davey/archive-projects.php
+++ themes/sacre-davey/archive-projects.php
@@ -13,21 +13,24 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
+				
 				<?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
+				</div>
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
+			<div class="projects-grid">
 			<?php while ( have_posts() ) : the_post(); ?>
-
+				
 				<?php
 					get_template_part( 'template-parts/content' );
 				?>
 
 			<?php endwhile; ?>
-
+			</div>
 		<?php else : ?>
 
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
