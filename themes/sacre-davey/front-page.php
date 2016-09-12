44ec25458dcbd3a25293726cc5d153bffe30f79e
lisath92 <lisa.hsu.419@gmail.com>
2016-09-12 00:01:04 -0700

    add front page intro and partner logos

--- themes/sacre-davey/front-page.php
+++ themes/sacre-davey/front-page.php
@@ -14,10 +14,25 @@ get_header(); ?>
 				<?php the_post_thumbnail( 'full' ); ?>
 			<?php endif; ?>
 		</section>
-		
+		<section class="intro">
+			<p>Sacré-Davey provides engineering and project management services. 
+				We offer professional advice and technical expertise that help you 
+				improve your return on investments.</p>
+				<img src="<?php echo get_template_directory_uri() .'/assets/images/diamond.png'?>" alt="">
+				<p>
+					We work hard to develop a deep understanding of your needs 
+					and objectives, and to provide you with the personal service you deserve.</p>
+				</section>
+			<section class="partners">
+			<h3>Partners</h3>
+			<img src="<?php echo get_template_directory_uri() .'/assets/images/Biochem Logo.png'?>" alt="Biochem partner logo">
+			<img src="<?php echo get_template_directory_uri() .'/assets/images/EngineeringLogo.png'?>" alt="Engineering partner logo">
+			<img src="<?php echo get_template_directory_uri() .'/assets/images/FerencoLogo.png'?>" alt="Ferenco partner logo">
+			<img src="<?php echo get_template_directory_uri() .'/assets/images/HTEClogo.png'?>" alt="HTEC partner logo">
+			</section>
 
 
-	</main><!-- #main -->
-</div><!-- #primary -->
+			</main><!-- #main -->
+		</div><!-- #primary -->
 
-<?php get_footer(); ?>
+		<?php get_footer(); ?>
