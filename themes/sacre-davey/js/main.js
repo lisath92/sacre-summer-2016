1a7a685e862dcde133a5ac89db24373be86e0a53
lisath92 <lisa.hsu.419@gmail.com>
2016-09-11 18:37:15 -0700

    open nav bar menu turns body color gray

--- themes/sacre-davey/js/main.js
+++ themes/sacre-davey/js/main.js
@@ -3,18 +3,22 @@
 //Toggle hamburger menu
   var mainMenu = $('.menu-main-navigation-container');
 
-  $('.fa-bars').on('click', function(){
+  $('body').on('click', function(event){
+  // $('.fa-bars').on('click', function(){
     event.preventDefault();
-    if (mainMenu.is(":hidden")) {
+    if($(event.target).hasClass('fa-bars') && mainMenu.is(":hidden")){ 
+    // if (mainMenu.is(":hidden")) {
       mainMenu.slideDown('slow');
+      $('body').css("background-color","rgba(0,0,0,0.4)");
   } else {
     mainMenu.slideUp('slow');
-  }
-})
+    $('body').css("background-color","rgba(0,0,0,0)");
+  } 
+});
   
 //Show Menu if more than 600px view width
   $(window).resize(function(){
-    if($(window).width()>600) {
+    if($(window).width()>582) {
       mainMenu.show();
     } else {
       mainMenu.hide();
