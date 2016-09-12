30a2d42272bc0419230f8c305ed6d6666d8e7263
lisath92 <lisa.hsu.419@gmail.com>
2016-09-11 22:45:55 -0700

    add front page header image

--- themes/sacre-davey/front-page.php
+++ themes/sacre-davey/front-page.php
@@ -7,34 +7,17 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-	This is the front page!
-		<?php if ( have_posts() ) : ?>
-
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<section class="hero-image">
+			<?php if ( has_post_thumbnail() ) : ?>
+				<?php the_post_thumbnail( 'full' ); ?>
 			<?php endif; ?>
+		</section>
+		
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content' ); ?>
+	</main><!-- #main -->
+</div><!-- #primary -->
 
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- themes/sacre-davey/js/main.js
+++ themes/sacre-davey/js/main.js
@@ -1,26 +1,27 @@
 (function($) {
 
 //Toggle hamburger menu
-  var mainMenu = $('.menu-main-navigation-container');
+var mainMenu = $('.menu-main-navigation-container');
 
-  $('body').on('click', function(event){
-    if($(event.target).hasClass('fa-bars') && mainMenu.is(":hidden")){ 
-      mainMenu.slideDown('slow');
-      $('body').css("background-color","rgba(0,0,0,0.4)");
-      mainMenu.submit();
-  } else {
+$('body').on('click', function(event){
+  if($(event.target).hasClass('fa-bars') && mainMenu.is(":hidden")){ 
+    mainMenu.slideDown('slow');
+    $('body').css("background-color","rgba(0,0,0,0.4)");
+    mainMenu.submit();
+  } else if($(window).width()<582){
     mainMenu.slideUp('slow');
     $('body').css("background-color","rgba(0,0,0,0)");
+
   } 
 });
-  
+
 //Show Menu if more than 600px view width
-  $(window).resize(function(){
-    if($(window).width()>582) {
-      mainMenu.show();
-    } else {
-      mainMenu.hide();
-    }
-  })
+$(window).resize(function(){
+  if($(window).width()>582) {
+    mainMenu.show();
+  } else {
+    mainMenu.hide();
+  }
+})
 
 })(jQuery);
