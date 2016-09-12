de89a52617cf39c399296ac346223542366d5a34
lisath92 <lisa.hsu.419@gmail.com>
2016-09-11 18:41:34 -0700

    remove commented out codes

--- themes/sacre-davey/page-services.php
+++ themes/sacre-davey/page-services.php
@@ -13,9 +13,6 @@ get_header(); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<header class="entry-header">
-					<?php //the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-				</header>
 
 				<div class="entry-content">
 					<h2>One-stop-shop for all your engineering needs</h2>
