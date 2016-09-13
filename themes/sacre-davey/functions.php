1aa35e13ad967386187d4e85113d6814ab5beb84
lisath92 <lisa.hsu.419@gmail.com>
2016-09-13 13:59:36 -0700

    register another menu for about us page

--- themes/sacre-davey/functions.php
+++ themes/sacre-davey/functions.php
@@ -24,6 +24,7 @@ function sacre_davey_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => esc_html( 'Primary Menu' ),
+		'about-us' => esc_html( 'About Us' ),
 	) );
 
 	// Switch search form, comment form, and comments to output valid HTML5.
