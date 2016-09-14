3c2d233c3ecea7e41af1f2bce9a36aaa1d3d868d
fergmac <f.macconnell@gmail.com>
2016-09-13 22:35:36 -0700

    add shortcode for careers page gallery into php

--- themes/sacre-davey/page-careers.php
+++ themes/sacre-davey/page-careers.php
@@ -24,6 +24,7 @@ wp_nav_menu( array( 'theme_location' => 'about-us', 'container_class' => 'about-
 				<div class="growing-text">
 					<p>Our established engineering firm is GROWING! With our home office in North Vancouver and regional offices across Canada and the Western United States, we are looking for dynamic, well rounded individuals to join our team. These positions require an individual who is highly motivated, self-directed and has amazing attention to detail. We’re always looking for talented individuals to join our team in making a difference.</p>
 				</div>
+				<?php echo do_shortcode("[robo-gallery id='51']"); ?>
 				<div class="life-work-banner">
 					<h2>Join us and put a little more life in your life/work</h2>
 					<p>Sacré Davey is our home not just where we work, we are a family and we want you to be apart of it. Come join us for a challenging yet rewarding work experience where you'll be able to learn and move your career forward.</p>
