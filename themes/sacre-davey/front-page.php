22867b609c22323c56bd07d8ddc45b29f664d909
fergmac <f.macconnell@gmail.com>
2016-09-14 21:31:50 -0700

    style mobile project block on front page, add excerpt block and linear-gradient

--- themes/sacre-davey/front-page.php
+++ themes/sacre-davey/front-page.php
@@ -37,7 +37,7 @@ get_header(); ?>
    					<div class="projects-block-info">
 
    						<a><h1><?php the_title(); ?></h1></a>
-   						<p><?php the_content(); ?></p>
+   						<p><?php the_excerpt(); ?></p>
 
    					</div>	
    				</div>
