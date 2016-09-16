bcc1df4b3425d0c89c3bd10291d9bc8772fe44fa
lisath92 <lisa.hsu.419@gmail.com>
2016-09-16 11:44:03 -0700

    add read less button to read-more section and allow toggling close of the section on click

--- themes/sacre-davey/js/main.js
+++ themes/sacre-davey/js/main.js
@@ -1,67 +1,66 @@
 (function($) {
 
 
-    var mainMenu = $('.nav-mobile'),
-    $carousel = $('.carousel').flickity({
-        cellAlign: 'center',
-        contain: true,
-        prevNextButtons: false
-    }),
-    projectDescription = $('.entry-content');
+  var mainMenu = $('.nav-mobile'),
+  $carousel = $('.carousel').flickity({
+    cellAlign: 'center',
+    contain: true,
+    prevNextButtons: false
+  }),
+  projectDescription = $('.entry-content');
 
     //Toggle hamburger menu
     $('body').on('click', function(event) {
-      console.log($(event.target));
-        if ($(event.target).hasClass('fa-bars') && mainMenu.is(":hidden")) {
-            mainMenu.slideDown('slow');
-            $('body').css("background-color", "rgba(0,0,0,0.4)");
-            mainMenu.submit();
-        } else if ($(window).width() < 582) {
-            mainMenu.slideUp('slow');
-            $('body').css("background-color", "rgba(0,0,0,0)");
+      if ($(event.target).hasClass('fa-bars') && mainMenu.is(":hidden")) {
+        mainMenu.slideDown('slow');
+        $('body').css("background-color", "rgba(0,0,0,0.4)");
+        mainMenu.submit();
+      } else if ($(window).width() < 582) {
+        mainMenu.slideUp('slow');
+        $('body').css("background-color", "rgba(0,0,0,0)");
+      }
 
-        }
+      if ($(event.target).hasClass('entry-title') && $(window).width() < 582) {
+        projectDescription.css("display", "block");
 
-        if ($(event.target).hasClass('entry-title') && $(window).width() < 582) {
-            projectDescription.css("display", "block");
-
-        } else if ($(event.target).hasClass('fa-times')) {
-            console.log('hey');
-            projectDescription.css("display", "none");
-        }
-
-        if($(event.target).hasClass('read-more-button')) {
-          $('.read-more').show();
-        }
+      } else if ($(event.target).hasClass('fa-times')) {
+        projectDescription.css("display", "none");
+      }
     });
 
     //Show Sub Sectors Lists
     $('.sub-sector-title').on('click', function() {
-        var subSectorList = $(this).next('div');
-        subSectorList.slideToggle('fast', function() {
-            if (subSectorList.is(':visible')) {
-                $(this).prev('div').addClass('new-sub-sector-title');
-            } else {
-                $(this).prev('div').removeClass('new-sub-sector-title');
-            }
-        })
+      var subSectorList = $(this).next('div');
+      subSectorList.slideToggle('fast', function() {
+        if (subSectorList.is(':visible')) {
+          $(this).prev('div').addClass('new-sub-sector-title');
+        } else {
+          $(this).prev('div').removeClass('new-sub-sector-title');
+        }
+      })
     });
 
     if($('body').hasClass('postid-67')) {
       $('.read-more').toggleClass('style-1');
-  }
+    }
 
-  var count = $('section.development').length,
-  button=$('.read-more-button');
- 
+    var count = $('section.development').length,
+    button=$('.read-more-button');
 
+    if(count< 2) {
+      button.hide();
+      $('.read-more').show();
+    };
 
-  if(count< 2) {
-    button.hide();
-    $('.read-more').show();
-  } 
-
-
-
-
-})(jQuery);
+    button.click(function(){
+      var currentDiv = $(this).parent('.development').next('div');
+      currentDiv.slideDown('slow');
+      if (currentDiv.find('div').length === 0){
+        currentDiv.append('<div class="read-less-button">Read Less</div>');
+        
+        $('.read-less-button').click(function(){
+          $(this).parent('div').slideUp('slow');
+          });
+      }
+    });
+  })(jQuery);
