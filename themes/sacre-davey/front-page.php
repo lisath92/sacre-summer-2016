a469791a47c9ce166e63985ec893018fc82e90ea
lisath92 <lisa.hsu.419@gmail.com>
2016-09-19 00:15:54 -0700

    add sectors icons to front-page

--- themes/sacre-davey/front-page.php
+++ themes/sacre-davey/front-page.php
@@ -26,8 +26,8 @@ get_header(); ?>
 
 				<?php //sacre-davey project post types on front page
 				$args = array( 'post_type' => 'projects', 'posts_per_page'   => 3, 
-            'order' => 'ASC'
-             );
+               'order' => 'ASC'
+               );
 
    				$projects_posts = get_posts( $args ); // returns an array of posts
    				?>
@@ -36,11 +36,11 @@ get_header(); ?>
                "contain": true,
                "pageDots": false,
                "contain": true }'>
-                <?php foreach ( $projects_posts as $post ) : setup_postdata( $post ); ?>
+               <?php foreach ( $projects_posts as $post ) : setup_postdata( $post ); ?>
 
-                   <div class="home-gallery-cell">
-                    <div class="projects-block-image"><?php the_post_thumbnail(); ?>
-                       <div class="projects-block-info">
+                 <div class="home-gallery-cell">
+                   <div class="projects-block-image"><?php the_post_thumbnail(); ?>
+                      <div class="projects-block-info">
 
                         <?php the_title(sprintf('<h1><a href="%s">', esc_url(get_permalink() ) ), '</a></h1>'); ?>
                         <?php the_excerpt(); ?>
@@ -49,17 +49,37 @@ get_header(); ?>
                </div>
             <?php endforeach; wp_reset_postdata(); ?>
          </div>
-      </div>
+         <section class="sector-icons">
+            <h1>Sectors</h1>
+            <div class="sector-categories">
+               <?php 
+               $category_arr = array(
+                  'taxonomy' => 'project_type',
+                  'hide_empty' => false,
+                  'order'=> 'ASC',
+                  'orderby'=>'id');
+               $sector_categories = get_terms($category_arr);
+               foreach ($sector_categories as $each_category): ?>
+               <div class="each-category">
+                  <img src="
+                  <?php echo get_template_directory_uri().'/assets/images/'.$each_category->slug .'.png';?>" alt=""/>
+                  <div class="sector-description">
+                  <?php echo $each_category->description; ?>
+                  </div>
+               </div>
+            <?php endforeach; ?>
+
+
+         </div>      
+      </section>
       <section class="partners">
-       <h3>Partners</h3>
-       <img src="<?php echo get_template_directory_uri() .'/assets/images/Biochem Logo.png'?>" alt="Biochem partner logo">
-       <img src="<?php echo get_template_directory_uri() .'/assets/images/EngineeringLogo.png'?>" alt="Engineering partner logo">
-       <img src="<?php echo get_template_directory_uri() .'/assets/images/FerencoLogo.png'?>" alt="Ferenco partner logo">
-       <img src="<?php echo get_template_directory_uri() .'/assets/images/HTEClogo.png'?>" alt="HTEC partner logo">
-    </section>
-
-
- </main><!-- #main -->
+        <h3>Partners</h3>
+        <img src="<?php echo get_template_directory_uri() .'/assets/images/Biochem Logo.png'?>" alt="Biochem partner logo">
+        <img src="<?php echo get_template_directory_uri() .'/assets/images/EngineeringLogo.png'?>" alt="Engineering partner logo">
+        <img src="<?php echo get_template_directory_uri() .'/assets/images/FerencoLogo.png'?>" alt="Ferenco partner logo">
+        <img src="<?php echo get_template_directory_uri() .'/assets/images/HTEClogo.png'?>" alt="HTEC partner logo">
+     </section>
+  </main><!-- #main -->
 </div><!-- #primary -->
 
 <?php get_footer(); ?>
