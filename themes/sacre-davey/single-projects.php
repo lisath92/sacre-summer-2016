9049830119cba5b28f74dcda526eaefa00dc38f9
fergmac <f.macconnell@gmail.com>
2016-09-19 20:58:42 -0700

    style single products page

--- themes/sacre-davey/single-projects.php
+++ themes/sacre-davey/single-projects.php
@@ -1,4 +1,4 @@
-<?php
+	<?php
 /**
  * The template for displaying all single projects.
  *
@@ -11,39 +11,24 @@ get_header(); ?>
 	<main id="main" class="site-main" role="main">
 		<div class="project-description-banner">
 
-		<p>For a detailed description of a project please enter the information below</p>
-		<input type="text" placeholder="Project Name"> 
-		<input type="text" placeholder="Email">
-		<submit class="project-description-button">Send</submit>
+			<p>For a detailed description of a project please enter the information below</p>
+			<input type="text" placeholder="Project Name"> 
+			<input type="text" placeholder="Email">
+			<submit class="project-description-button">Send</submit>
 		</div>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<div class="project">
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<header class="entry-header">
-
-					<?php if ( has_post_thumbnail() ) : ?>
-						<?php the_post_thumbnail( 'large' ); ?>
-					<?php endif; ?>
-
-					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-					
-				</header><!-- .entry-header -->
-
-				<div class="entry-content">
-					
+		<section class="home-gallery-cell">
+			<div class="projects-block-image"><?php the_post_thumbnail(); ?>
+				<div class="projects-block-info">
+					 <?php the_title(sprintf('<h1><a href="%s">', esc_url(get_permalink() ) ), '</a></h1>'); ?>
+					<?php the_excerpt(); ?>
 					<?php the_content(); ?>
-						
-					</div><!-- .entry-content -->
-
-					
-				</article><!-- #post-## -->
-
 				</div>
-			<?php endwhile; // End of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			</div>
+		</section><!-- .entry-header -->
 
 
-	<?php get_footer(); ?>
+</main><!-- #main -->
+</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
