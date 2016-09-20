2bf7f78dfe9d4d279c662b1dec3d9a304026bd0b
lisath92 <lisa.hsu.419@gmail.com>
2016-09-20 09:29:19 -0700

    try to add mobile view menu to sectors page

--- themes/sacre-davey/single-sector.php
+++ themes/sacre-davey/single-sector.php
@@ -7,7 +7,6 @@
 
 get_header(); 
 wp_nav_menu( array('theme_location' => 'sector-posts', 'container_class' => 'sector-menu' ) ); ?>
-
 <div id="primary" class="content-area">
   <main id="main" class="site-main" role="main">
     <?php while ( have_posts() ) : the_post(); ?>
