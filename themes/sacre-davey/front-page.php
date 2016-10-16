6227592aafa3023851c0356f095e33f1e88d5b38
Lisa <lisa.hsu.419@gmail.com>
2016-10-16 14:06:04 -0700

    change partners to affiliated companies and center footer logo

--- themes/sacre-davey/front-page.php
+++ themes/sacre-davey/front-page.php
@@ -79,7 +79,7 @@ get_header(); ?>
          <!-- </div>       -->
       </section>
       <section class="partners">
-        <h3>Partners</h3>
+        <h3>Affiliated Companies</h3>
         <img src="<?php echo get_template_directory_uri() .'/assets/images/Biochem Logo.png'?>" alt="Biochem partner logo">
         <img src="<?php echo get_template_directory_uri() .'/assets/images/EngineeringLogo.png'?>" alt="Engineering partner logo">
         <img src="<?php echo get_template_directory_uri() .'/assets/images/FerencoLogo.png'?>" alt="Ferenco partner logo">
