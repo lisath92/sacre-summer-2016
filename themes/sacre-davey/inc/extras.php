0d40a8295c607af7c1e35a43b1ecb6781ff90623
Lisa <lisa.hsu.419@gmail.com>
2016-10-19 22:29:40 -0700

    add custom colors to color palette and add more padding to sectors description

--- themes/sacre-davey/inc/extras.php
+++ themes/sacre-davey/inc/extras.php
@@ -176,14 +176,13 @@ function sacre_davey_sectors_inline_styles() {
         url('.$logo.') center no-repeat;
         height: 100%;
         width: 100%;
-        background-size: cover;
+        background-size: 100% 100%;
         
           }
     ';
     $counter++;
-  
-wp_add_inline_style('sacre-davey-style', $css);
 }
+wp_add_inline_style('sacre-davey-style', $css);
 }
 add_action( 'wp_enqueue_scripts','sacre_davey_sectors_inline_styles');  
 
@@ -225,3 +224,57 @@ function wpcodex_hide_email_shortcode( $atts , $content = null ) {
   return '<a href="mailto:' . antispambot( $content ) . '">' . antispambot( $content ) . '</a>';
 }
 add_shortcode( 'email', 'wpcodex_hide_email_shortcode' );
+
+//Add custom theme colors to the color palette
+function mytheme_change_tinymce_colors( $init ) {
+   $default_colours = '
+        "000000", "Black",
+        "993300", "Burnt orange",
+        "333300", "Dark olive",
+        "003300", "Dark green",
+        "003366", "Dark azure",
+        "000080", "Navy Blue",
+        "333399", "Indigo",
+        "333333", "Very dark gray",
+        "800000", "Maroon",
+        "FF6600", "Orange",
+        "808000", "Olive",
+        "008000", "Green",
+        "008080", "Teal",
+        "0000FF", "Blue",
+        "666699", "Grayish blue",
+        "808080", "Gray",
+        "FF0000", "Red",
+        "FF9900", "Amber",
+        "99CC00", "Yellow green",
+        "339966", "Sea green",
+        "33CCCC", "Turquoise",
+        "3366FF", "Royal blue",
+        "800080", "Purple",
+        "999999", "Medium gray",
+        "FF00FF", "Magenta",
+        "FFCC00", "Gold",
+        "FFFF00", "Yellow",
+        "00FF00", "Lime",
+        "00FFFF", "Aqua",
+        "00CCFF", "Sky blue",
+        "993366", "Brown",
+        "C0C0C0", "Silver",
+        "FF99CC", "Pink",
+        "FFCC99", "Peach",
+        "FFFF99", "Light yellow",
+        "CCFFCC", "Pale green",
+        "CCFFFF", "Pale cyan",
+        "99CCFF", "Light sky blue",
+        "CC99FF", "Plum",
+        "FFFFFF", "White"
+        ';
+    $custom_colours = '
+        "272D66", "Theme Dark Blue",
+        "279AB6", "Theme Light Blue"
+        ';
+    $init['textcolor_map'] = '['.$default_colours.','.$custom_colours.']';
+    $init['textcolor_rows'] = 6; // expand colour grid to 6 rows
+    return $init;
+}
+add_filter('tiny_mce_before_init', 'mytheme_change_tinymce_colors');
