9e8038f80aa5d6fb92c411f3a2b00b5ea44b32b7
Lisa <lisa.hsu.419@gmail.com>
2016-10-20 17:22:41 -0700

    fix projects archive page nav menu to match the other nav

--- themes/sacre-davey/archive-projects.php
+++ themes/sacre-davey/archive-projects.php
@@ -10,7 +10,7 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 		<div class="project-icon-toggle">
-			<p>Projects</p>
+			<span>Projects</span>
 			<i class="fa fa-angle-down" aria-hidden="true"></i>
 		</div>
 		<section class="mobile-project-nav-list">
