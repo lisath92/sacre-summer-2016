af1ce51ebb3012c3317823cdc6217fff3865bcab
Lisa Hsu <lisa.hsu.419@gmail.com>
2016-10-26 15:51:21 -0700

    clean up js file

--- themes/sacre-davey/js/menu-click-functions.js
+++ themes/sacre-davey/js/menu-click-functions.js
@@ -8,28 +8,28 @@ projectNav = $('.mobile-project-nav-list ul');
 $('body').on('click', function(event){
   
   //Main mobile nav menu
-  if($(event.target).hasClass('fa-bars')&&mainMenu.is(":hidden")) {
+  if($(event.target).hasClass('fa-bars')&&mainMenu.is(':hidden')) {
     mainMenu.slideDown('slow');
-    $('body').css("background-color", "rgba(0,0,0,0.4)");
+    $('body').css('background-color', 'rgba(0,0,0,0.4)');
   } else {
     mainMenu.slideUp('slow');
-    $('body').css("background-color", "rgba(0,0,0,0)");
+    $('body').css('background-color', 'rgba(0,0,0,0)');
   }
 
   //Sector mobile sub menu
-  if($('body').hasClass('single-sector') && $(event.target).hasClass('fa-angle-down') && sectorNav.is(":hidden")) {
+  if($('body').hasClass('single-sector') && $(event.target).hasClass('fa-angle-down') && sectorNav.is(':hidden')) {
     sectorNav.slideDown('slow');
   } else {
     sectorNav.slideUp('slow');
-    $('body').css("background-color", "rgba(0,0,0,0)");
+    $('body').css('background-color', 'rgba(0,0,0,0)');
   }
 
   //Projects mobile sub menu
-   if(($('body').hasClass('archive')||$('body').hasClass('single-projects')) && $(event.target).hasClass('fa-angle-down')&& projectNav.is(":hidden")) {
+   if(($('body').hasClass('archive')||$('body').hasClass('single-projects')) && $(event.target).hasClass('fa-angle-down')&& projectNav.is(':hidden')) {
     projectNav.slideDown('slow');
   } else {
     projectNav.slideUp('slow');
-    $('body').css("background-color", "rgba(0,0,0,0)");
+    $('body').css('background-color', 'rgba(0,0,0,0)');
   }
 });
 
