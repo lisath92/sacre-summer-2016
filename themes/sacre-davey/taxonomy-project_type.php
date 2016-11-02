d9980d842e7ce094637d8904df9da565eea2a4f7
Lisa Hsu <lisa.hsu.419@gmail.com>
2016-11-02 14:14:09 -0700

    add menu to the projects archive page and project taxonomy pages

--- themes/sacre-davey/taxonomy-project_type.php
+++ themes/sacre-davey/taxonomy-project_type.php
@@ -5,53 +5,14 @@
  * @package Sacre Davey Theme
  */
 
-get_header(); ?>
+get_header(); 
+wp_nav_menu( array('theme_location' => 'project-types', 'container_class' => 'project-type' ) ); ?>
+<div class="mobile-nav-title"><span>Projects</span><i class="fa fa-angle-down" aria-hidden="true"></i></div>
+<?php wp_nav_menu( array('theme_location' => 'project-mobile', 'container_class' => 'project-mobile') ); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-		<div class="project-icon-toggle">
-			<p>Projects</p>
-			<i class="fa fa-angle-down" aria-hidden="true"></i>
-		</div>
-		<section class="mobile-project-nav-list">
-
-			<?php  
-			$arg = array( 'taxonomy' => 'project_type', 'hide_empty' => false);
-
-			$terms = get_terms( $arg ); 
-			?>
-			<ul class="">
-				<li><a href="<?php echo get_post_type_archive_link('projects'); ?>">All</a></li>
-				<?php foreach ( $terms as $term ) : ?>
-
-					<li><a href=" <?php echo get_term_link($term); ?> " ><?php echo $term->name
-						?></a>
-					</li> 
-
-				<?php endforeach; ?>
-			</ul>
-
-
-		</section>
-		<section class="project-nav-list">
-
-			<?php  
-			$arg = array( 'taxonomy' => 'project_type', 'hide_empty' => false);
-
-			$terms = get_terms( $arg ); 
-			?>
-			<ul class="">
-				<li><a href="<?php echo get_post_type_archive_link('projects'); ?>">All</a></li>
-				<?php foreach ( $terms as $term ) : ?>
-
-					<li><a href=" <?php echo get_term_link($term); ?> " ><?php echo $term->name
-						?></a>
-					</li> 
-
-				<?php endforeach; ?>
-			</ul>
-
-		</section>
+		
 		<div class="project-description-banner">
 			<p>For a detailed description of a project please enter the information below</p>
 			<input type="text" placeholder="Project Name"><input type="text" placeholder="Email">
