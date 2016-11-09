ede200418367170f9e1776e1fb6fbb3d4c9837a4
Lisa Hsu <lisa.hsu.419@gmail.com>
2016-11-09 12:50:41 -0800

    change length of excerpt and the [...] to 'read more'

--- themes/sacre-davey/inc/extras.php
+++ themes/sacre-davey/inc/extras.php
@@ -265,13 +265,43 @@ function sacre_davey_modifying_project_archive_query($query) {
 }
 add_action( 'pre_get_posts', 'sacre_davey_modifying_project_archive_query' );
 
-/*
-* Limit number of words for excerpts
-*/
-function custom_excerpt_length( $length ) {
-  return 25;
+
+//Change the excerpt length, [....] and allowed tags
+
+function custom_wp_trim_excerpt($text) {
+$raw_excerpt = $text;
+if ( '' == $text ) {
+    //Retrieve the post content. 
+    $text = get_the_content('');
+ 
+    //Delete all shortcode tags from the content. 
+    $text = strip_shortcodes( $text );
+ 
+    $text = apply_filters('the_content', $text);
+    $text = str_replace(']]>', ']]&gt;', $text);
+     
+    $allowed_tags = '<p>'; /*** MODIFY THIS. Add the allowed HTML tags separated by a comma.***/
+    $text = strip_tags($text, $allowed_tags);
+     
+    $excerpt_word_count = 70; 
+    $excerpt_length = apply_filters('excerpt_length', $excerpt_word_count); 
+     
+    $excerpt_end = 'Read more'; 
+    $excerpt_more = apply_filters('excerpt_more', ' ' . $excerpt_end);
+     
+    $words = preg_split("/[\n\r\t ]+/", $text, $excerpt_length + 1, PREG_SPLIT_NO_EMPTY);
+    if ( count($words) > $excerpt_length ) {
+        array_pop($words);
+        $text = implode(' ', $words);
+        $text = $text . '...' . $excerpt_more;
+    } else {
+        $text = implode(' ', $words);
+    }
 }
-add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+return apply_filters('wp_trim_excerpt', $text, $raw_excerpt);
+}
+remove_filter('get_the_excerpt', 'wp_trim_excerpt');
+add_filter('get_the_excerpt', 'custom_wp_trim_excerpt');
 
 
 /**
