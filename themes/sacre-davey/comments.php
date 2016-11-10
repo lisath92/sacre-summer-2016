29e22b5a70470bb5947d33b5b967299e58315588
Lisa Hsu <lisa.hsu.419@gmail.com>
2016-11-09 22:43:37 -0800

    remove website input field

--- themes/sacre-davey/comments.php
+++ themes/sacre-davey/comments.php
@@ -20,9 +20,9 @@ if ( post_password_required() ) {
 	<?php // You can start editing here -- including this comment! ?>
 
 	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php esc_html( comments_number( '0 Comments', '1 Comment', '% Comments' ) ); ?>
-		</h2>
+		<!-- <h2 class="comments-title">
+			<?php //esc_html( comments_number( '0 Comments', '1 Comment', '% Comments' ) ); ?>
+		</h2> -->
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 			<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
@@ -73,15 +73,15 @@ if ( post_password_required() ) {
 		'label_submit'         => esc_html( 'Submit' ),
 		'cancel_reply_link'    => esc_html( '[Cancel reply]' ),
 		'fields' => apply_filters('comment_form_default_fields', array (
-			'author' => '<p class="comment-form-author">' . '<label for="author">' . __( 'Your Name' ) . '<span>*<span></label> ' .
+			'author' => '<p class="comment-form-author">' . '<label for="author">' . __( 'Your Name', 'domainreference') . '<span>*<span></label> ' .
 
-			'<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' /></p>',   
+			'<input id="author" name="author" type="text" /></p>',   
 
 			'email'  => '<p class="comment-form-email">' .
 
-			'<label for="email">' . __( 'Your Email' ) . '<span>*<span></label> ' .
+			'<label for="email">' . __( 'Your Email', 'domainreference' ) . '<span>*<span></label> ' .
 
-			'<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' />'.'</p>',
+			'<input id="email" name="email" type="text" /></p>',
 
 			'url'    => '' ) ),
 			) ); ?>
