5238e9ebfd90e9d24b61fa2543f35109f5878e93
Lisa Hsu <lisa.hsu.419@gmail.com>
2016-11-24 20:34:45 +0100

    add section to project images

--- themes/sacre-davey/template-parts/content-projects.php
+++ themes/sacre-davey/template-parts/content-projects.php
@@ -22,10 +22,12 @@
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<header class="entry-header">
-						<?php if ( has_post_thumbnail() ) : ?>
-							<?php the_post_thumbnail( 'large' );
-							;?>
-						<?php endif; ?>
+						<div class="project-image">
+							<?php if ( has_post_thumbnail() ) : ?>
+								<?php the_post_thumbnail( 'large' );
+								;?>
+							<?php endif; ?>
+						</div>
 
 						<!-- Get slug name -->
 						<?php 
