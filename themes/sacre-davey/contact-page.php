20114a4baab548228addc3a9cd1715329e102d3d
Lisa Hsu <lisa.hsu.419@gmail.com>
2017-09-02 13:31:38 -0700

    refactor layout and add in TO address

--- themes/sacre-davey/contact-page.php
+++ themes/sacre-davey/contact-page.php
@@ -26,73 +26,79 @@ get_header(); ?>
 </article><!-- #post-## -->
 
 		<?php endwhile; // End of the loop. ?>
-
-		<div class="offices-wrapper container">
+		<section class="offices">
 			<div class="office-block">
 				<h2>Canada</h2>
-				<h3>Sacré-Davey Head Office</h3>
-				<div class="office-address">
-					<p>315 Mountain Hwy
-					North Vancouver, BC, V7J 2K7
-					Canada</p>
+				<div class="contact">
+					<h3>Sacré-Davey Head Office</h3>
+					<p>315 Mountain Hwy North Vancouver, BC, V7J 2K7</p>
+					<ul>
+						<li>Phone: (604) 986-0663</li>
+						<li>Fax: (604) 986-0525</li>
+						<li>Email: <a href="mailto:admin@sacre-davey.com">admin@sacre-davey.com</a></li>
+					</ul>
 				</div>
-				<ul class="office-contact">
-					<li>Phone: (604) 986-0663</li>
-					<li>Fax: (604) 986-0525</li>
-					<li>Email: <a href="mailto:admin@sacre-davey.com" target="blank">admin@sacre-davey.com</a></li>
-				</ul>
-				<h3>Sacré-Davey Calgary Office</h3>
-				<div class="office-address">
+			</div>
+			<div class="office-block">
+				<h2></h2>
+				<div class="contact">
+					<h3>Sacré-Davey Calgary Office</h3>
 					<p>435 – 4th Avenue SW, Suite 560, Calgary, AB T2P 3A8</p>
+					<ul>
+						<li>Phone: 403-455-2583</li>
+						<li>Email: <a href="mailto:admin-ab@sacre-davey.com">admin-ab@sacre-davey.com</a></li>
+					</ul>
 				</div>
-				<ul class="office-contact">
-					<li>Phone: 403-455-2583</li>
-					<li>Email: <a href="mailto:admin-ab@sacre-davey.com">admin-ab@sacre-davey.com</a></li>
-				</ul>
+			</div>
+			<div class="office-block">
+				<h2></h2>
+				<div class="contact">
+					<h3>Sacré-Davey Toronto Office</h3>
+					<p>42425 Matheson Boulevard East, Suite 891 Mississauga, Toronto, ON, L4W 5K4</p>
+					<ul>
+						<li>Phone: 905-361-2598</li>
+					</ul>
+				</div>
 			</div>
 			<div class="office-block">
 				<h2>United States Office</h2>
-				<h3>SDE3</h3>
-				<div class="office-address">
-					<p>2200 Sixth Avenue
-					Suite #520
-					Seattle, WA 98121
-					USA</p>
+				<div class="contact">
+					<h3>SDE3</h3>
+					<p>2200 Sixth Avenue Suite #520 Seattle, WA 98121 USA</p>
+					<ul>
+						<li>Phone: 1-206-462-3600</li>
+						<li>Fax: 1-206-462-3599</li>
+						<li>Email: <a href="mailto:solutions@e3energypartners.com">solutions@e3energypartners.com</a></li>
+					</ul>
 				</div>
-				<ul class="office-contact">
-					<li>Phone: 1-206-462-3600</li>
-					<li>Fax: 1-206-462-3599</li>
-					<li>Email: <a href="mailto:solutions@e3energypartners.com">solutions@e3energypartners.com</a></li>
-				</ul>
-			</div>					
+			</div>
 			<div class="office-block">
 				<h2>Associated Offices</h2>
-				<h3>Sacré-Davey Group International</h3>
-				<div class="office-address">
-					<p>PO BOX 111751<br/>
-					Abu Dhabi<br/>
-					United Arab Emirates</p>
+					<div class="contact">
+					<h3>Sacré-Davey Group International</h3>
+					<p>PO BOX 111751<br/>Abu Dhabi<br/>United Arab<br/>Emirates</p>
+					<ul>
+						<li>Phone: 971-02-650-9440</li>
+						<li>Fax: 971-02-650-9441</li>
+						<li>Email: <a href="mailto:admin-me@sacre-davey.com">admin-me@sacre-davey.com</a></li>
+					</ul>
 				</div>
-				<ul class="office-contact">
-					<li>Phone: 971-02-650-9440</li>
-					<li>Fax: 971-02-650-9441</li>
-					<li>Email: <a href="mailto:admin-me@sacre-davey.com">admin-me@sacre-davey.com</a></li>
-				</ul>
-			</div>	
+			</div>
 			<div class="office-block">
 				<h2>Canadian Regional Offices</h2>
-				<div class="office-address">
-					<p>Toronto, Ontario<br/>
-					Regina, Saskatchewan</p>
+				<div class="contact">
+				<h3></h3>
+					<ul>
+						<li>Regina, Saskatchewan</li>
+					</ul>
+					<p>Please contact our Head Office for regional office information</p>
+					<ul>
+						<li>Phone: 604-986-0663</li>
+						<li>Email: <a href="mailto:admin@sacre-davey.com">admin@sacre-davey.com</a></li>
+					</ul>
 				</div>
-				<ul class="office-contact">
-					<li>Please contact our Head Office for regional office information</li>
-					<li>Phone: 604-986-0663</li>
-					<li>Email: <a href="mailto:admin@sacre-davey.com">admin@sacre-davey.com</a></li>
-				</ul>
-
 			</div>
-		</div>
+		</section>
 	</main><!-- #main -->
 </div><!-- #primary -->
 
